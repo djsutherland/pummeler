c533f4cf4e717be4b50020582dfb3036d0550b57
Dougal J. Sutherland <dougal@gmail.com>
2016-11-08 14:39:24 +0000

    fix bug in get_dummies when excluding real features

--- pummeler/featurize.py
+++ pummeler/featurize.py
@@ -32,8 +32,8 @@ def get_dummies(df, stats, num_feats=None, ret_df=True, skip_feats=None,
 
     reals = out[:, :len(real_feats)]
     reals[:] = df[real_feats]
-    reals[:] -= stats['real_means']
-    reals[:] /= stats['real_stds']
+    reals[:] -= stats['real_means'][real_feats]
+    reals[:] /= stats['real_stds'][real_feats]
     reals[np.isnan(reals)] = 0
     if ret_df:
         feat_names = list(real_feats)
