a1c4407122967509a7c17b63c9a42e8fc97a7908
Dougal J. Sutherland <dougal@gmail.com>
2020-02-18 14:29:44 -0600

    easier utility for merging embeddings from code

--- pummeler/misc.py
+++ pummeler/misc.py
@@ -1,27 +1,22 @@
 from __future__ import print_function, division
-import sys
 
 import numpy as np
 
 from .data import geocode_data
 
 
-def _get_merged_embeddings(data_dict, mapping_fn, out_prefix):
-    region_names = data_dict["region_names"]
-    region_weights = data_dict["region_weights"]
-
+def merge_embeddings(embeddings, region_names, region_weights, region_maps):
     squeezed = region_weights.ndim == 1
     if squeezed:
         region_weights = region_weights[:, np.newaxis]
 
     n_subsets = region_weights.shape[1]
 
-    mapped_names = [mapping_fn(r) for r in region_names]
-    m_names = sorted(set(mapped_names))
+    m_names = sorted(set(region_maps))
     m_names_lookup = {n: i for i, n in enumerate(m_names)}
 
     transform = np.zeros((len(m_names), len(region_names), n_subsets))
-    for r_i, (m, w) in enumerate(zip(mapped_names, region_weights)):
+    for r_i, (m, w) in enumerate(zip(region_maps, region_weights)):
         transform[m_names_lookup[m], r_i, :] = w
 
     m_weights = transform.sum(axis=1)
@@ -31,35 +26,45 @@ def _get_merged_embeddings(data_dict, mapping_fn, out_prefix):
     nz = np.broadcast_to((m_weights != 0)[:, np.newaxis, :], transform.shape)
     transform[nz] /= np.broadcast_to(m_weights[:, np.newaxis, :], transform.shape)[nz]
 
-    ret = {
-        "{}_names".format(out_prefix): m_names,
-        "{}_weights".format(out_prefix): m_weights,
-    }
-    for k in data_dict:
-        if k.startswith("emb_"):
-            print("Mapping {}...".format(k), end="", file=sys.stderr)
-            emb = data_dict[k]
-            if squeezed:
-                emb = emb[:, :, np.newaxis]
+    m_embeddings = []
+    for emb in embeddings:
+        if squeezed:
+            emb = emb[:, :, np.newaxis]
 
-            # need to do a matrix multiply for each subset:
-            #  - np.einsum('grs,rfs->gfs') would do this, but doesn't call BLAS
-            #  - rolling the subset axis to the front and calling np.matmul
-            #    would do this, but it just calls einsum anyway:
-            #    https://github.com/numpy/numpy/issues/7569
+        # need to do a matrix multiply for each subset:
+        #  - np.einsum('grs,rfs->gfs') would do this, but doesn't call BLAS
+        #  - rolling the subset axis to the front and calling np.matmul
+        #    would do this, but it just calls einsum anyway:
+        #    https://github.com/numpy/numpy/issues/7569
 
-            out = np.empty((n_subsets, len(m_names), emb.shape[1]))
-            for i in range(n_subsets):
-                np.dot(transform[:, :, i], emb[:, :, i], out=out[i])
-            ret[k] = np.rollaxis(out, 0, 3)
+        out = np.empty((n_subsets, len(m_names), emb.shape[1]))
+        for i in range(n_subsets):
+            np.dot(transform[:, :, i], emb[:, :, i], out=out[i])
+        m_emb = np.rollaxis(out, 0, 3)
+        if squeezed:
+            m_emb = m_emb[:, :, 0]
+        m_embeddings.append(m_emb)
 
-            if squeezed:
-                ret[k] = ret[k][:, :, 0]
-            print("done", file=sys.stderr)
-        elif k in {"region_names", "region_weights"}:
-            pass
-        else:
-            ret[k] = data_dict[k]
+    return m_embeddings, m_names, m_weights
+
+
+def _get_merged_embeddings(data_dict, mapping_fn, out_prefix):
+    emb_names, embeddings = zip(
+        *[(k, v) for k, v in data_dict.items() if k.startswith("emb_")]
+    )
+
+    m_embeddings, m_names, m_weights = merge_embeddings(
+        embeddings,
+        data_dict["region_names"],
+        data_dict["region_weights"],
+        [mapping_fn(r) for r in data_dict["region_names"]],
+    )
+
+    ret = data_dict.copy()
+    ret[f"{out_prefix}_names"] = m_names
+    ret[f"{out_prefix}_weights"] = m_weights
+    for n, e in zip(emb_names, m_embeddings):
+        ret[n] = e
     return ret
 
 
